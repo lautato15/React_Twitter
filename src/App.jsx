0ac09a990888f356f70a9dc3662ed225748033e0
JosePedroLavaggi <138890497+JosePedroLavaggi@users.noreply.github.com>
2023-09-02 18:39:52 -0300

    vistas

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Register from "./components/Register";
 import Login from "./components/Login";
+import Profile from "./components/Profile";
 function App() {
   return (
     <>
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/registro" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </>
   );
---
+++ src/components/Profile.jsx
@@ -0,0 +1,190 @@
+import { useEffect, useState } from "react";
+/* import UsersList from "./UsersList"; */
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import Sidebar from "./partials/Sidebar";
+import SidebarLeft from "./partials/SidebarLeft";
+import "../App.css";
+
+function Porfile(props) {
+  const [name, setName] = useState(null);
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
+
+  /*  useEffect(() => {
+    if (!user) return navigate("/login");
+  }, []); */
+  return (
+    <div className="container homeContainer">
+      <div className="row homeRow">
+        <SidebarLeft />
+        <div className="col-10 col-md-6 p-0">
+          <div className="divProfile">
+            <div className="divProfile">
+              {/*  { <% if (userShow.avatar.startsWith("http")) {%>}
+              <img className="img-fluid divProfile" src="<%= userShow.frontPage%>" alt="" />
+              <% } else{%>
+              <img className="img-fluid divProfile" src="/img/<%= userShow.frontPage%>" alt="" />
+              <%} %> */}
+            </div>
+            {/* <% if (userShow.avatar.startsWith("http")) {%>
+            <img className="imgProfile me-2" src="<%= userShow.avatar %>" alt="Image Profile" />
+            <% } else{%>
+            <img className="imgProfile me-2" src="/img/<%= userShow.avatar %>" alt="Image Profile" />
+            <%} %> */}
+          </div>
+          <div className="d-flex flex-column-reverse border-bottom">
+            <h2 className="container txtLg TweetProfile">Tweets</h2>
+            <div className="container">
+              <p>{/* {<%= userShow.biography %>} */}</p>
+            </div>
+            <div className="container py-2 d-flex flex-column-reverse">
+              <div className="mt-5">
+                <h2 className="txtLg mb-0 fw-bold">
+                  {/* <%= userShow.fullname %> */}
+                </h2>
+                <div className="d-flex">
+                  <p className="txtMd text-secondary m-0 flex-grow-1">
+                    @{/* <%= userShow.username %> */}
+                  </p>
+                  <a
+                    href="/followers/<%= userShow.id %>"
+                    className="txtSm text-secondary"
+                  >
+                    <span className="text-dark fw-bold">
+                      {/* <%= userShow.followers.length %> */}
+                    </span>
+                    Followers
+                  </a>
+                  <a
+                    href="/followings/<%= userShow.id %>"
+                    className="txtSm text-secondary"
+                  >
+                    <span className="text-dark fw-bold ms-2">
+                      {" "}
+                      {/* <%= userShow.followings.length %> */}{" "}
+                    </span>
+                    Followings
+                  </a>
+                </div>
+              </div>
+              <div className="d-flex justify-content-end mt-2">
+                {/*  {/*  <% if( userShow.id === user.id ) { %>
+                  <button className="buttonEditProfile btn rounded-pill txtMd">Edit Profile</button>
+                <% }else if( userShow.followers.includes(user.id) ){%>
+                <form action="/unfollow/<%= userShow.username %>?_method=PATCH" method="post">
+                  <button
+                    type="submit"
+                    className="btn buttonFollowing border border-black rounded-pill"
+                  >
+                    Following
+                  </button>
+                </form>
+                <% }else if(!userShow.followers.includes(user.id)){%>
+                <form action="/follow/<%= userShow.username %>?_method=PATCH" method="post">
+                  <button className="btn buttonEditProfile rounded-pill txtMd">Follow</button>
+                </form>
+                /* <% }%> */}
+              </div>
+            </div>
+          </div>
+          {/*   <!-- TWEETS -->
+          <% for(let tweet of userShow.tweets){ %> */}
+          <div className="container d-flex p-3 border-bottom">
+            <a href="" className="pt-2">
+              {/*  { <% if (userShow.avatar.startsWith("http")) {%>
+              <img
+                className="iconAccount rounded-pill me-2"
+                src="<%= userShow.avatar %>"
+                alt="Icon Account"
+              />
+              <% } else{%>
+              <img
+                className="iconAccount rounded-pill me-2"
+                src="/img/<%= userShow.avatar %>"
+                alt="Icon Account"
+              />
+              /* <%} %> */}
+            </a>
+            <div className="w-100">
+              <div className="d-flex">
+                <a href="" className="text-reset txtMd fw-semibold me-1">
+                  {" "}
+                  {/* <%= userShow.fullname %> */}{" "}
+                </a>
+                <a href="" className="txtMd usernameTweetText me-1">
+                  @{/* <%= userShow.username %> */}{" "}
+                </a>
+                <p className="txtMd usernameTweetText m-0">
+                  {/* <%= formatDistanceToNow(tweet.createdAt) %> */}
+                </p>
+              </div>
+              <p className="txtMd m-0">{/* <%= tweet.content %> */}</p>
+              <div className="d-flex">
+                {/*  <% if (!tweet.likes.includes(user.id)) { %> */}
+                <form
+                  className="d-flex justify-content-center align-items-center"
+                  action="/tweet/<%= tweet.id %>/like"
+                  method="post"
+                >
+                  <input type="hidden" name="userId" value="<%= user.id %>" />
+                  <button type="submit" className="btn p-0">
+                    <img
+                      className="img-fluid iconLike d-inline"
+                      src="/img/heartWithe.png"
+                      alt=""
+                    />
+                    <p className="d-inline">
+                      {/* <!-- CONTADOR DE LIKES -->
+                      <%= tweet.likes.length %> */}
+                    </p>
+                  </button>
+                </form>
+                {/*  {<% } else { %>/*}  */}
+                */{" "}
+                <form
+                  className="d-flex justify-content-center align-items-center"
+                  action="/tweet/<%= tweet.id %>/unLike"
+                  method="post"
+                >
+                  <input type="hidden" name="userId" value="<%= user.id %>" />
+                  <button type="submit" className="btn p-0">
+                    <img
+                      className="img-fluid iconLike d-inline"
+                      src="/img/heartRed.png"
+                      alt=""
+                    />
+                    <p className="d-inline">
+                      {/*   <!-- CONTADOR DE LIKES -->
+                      <%= tweet.likes.length %> */}
+                    </p>
+                  </button>
+                </form>
+                {/* /* <% } %> <% if (userShow.id === user.id) { %> */}
+                <form
+                  className="d-flex justify-content-center align-items-center ms-auto"
+                  action="/tweet/delete/<%= tweet.id %>?_method=DELETE"
+                  method="POST"
+                >
+                  <input type="hidden" name="tweet" value="<%=tweet.id %>>" />
+                  <button
+                    type="submit"
+                    className="btn d-flex justify-content-center align-items-center"
+                  >
+                    <i
+                      className="fas fa-trash" /* style="color: #dc3444" */
+                    ></i>
+                  </button>
+                </form>
+                {/* /* <% } %> */}
+              </div>
+            </div>
+          </div>
+          {/*  <% } %> */}
+        </div>
+        <Sidebar />
+      </div>
+    </div>
+  );
+}
+export default Porfile;
